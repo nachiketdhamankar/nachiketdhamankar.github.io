25ce11b3a4f7b1dca6ce3b2214dc9888b7849af1
Nachiket Dhamankar <nachiketdhamankar@users.noreply.github.com>
2017-03-05 11:06:27 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
 var  mn = $(".main-nav");
     mns = "main-nav-scrolled";
-    hdr = $('header').height();
-
+var test = $( window ).height();
 $(window).scroll(function() {
-  if( $(this).scrollTop() > hdr ) {
+  if( $(this).scrollTop() > test ) {
     mn.addClass(mns);
   } else {
     mn.removeClass(mns);
   }
+});
+$(function() {
+  var pull    = $('#pull');
+    menu    = $('#nav > .col-sm-2');
+    menuHeight  = menu.height();
+ 
+  $(pull).on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
+});
+$(window).resize(function(){
+  var w = $(window).width();
+  if(w > 320 && menu.is(':hidden')) {
+    menu.removeAttr('style');
+  }
+});
+$('#pull').on('click', function() {
+    $('#nav').toggleClass('clicked');
+    $('#navArrow').toggleClass('clicked');
 });
